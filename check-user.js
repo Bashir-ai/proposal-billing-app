f0a77f1a414f831d9138f170c967c0ec5b03ec7c
Bashir <Bashir1@192.168.1.2>
2026-01-07 20:15:47 -0300

    Clean up temporary test files

--- check-user.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.user.findUnique({ 
-  where: { email: 'bkv@vpa.pt' },
-  select: { email: true, password: true, role: true }
-}).then(user => {
-  console.log('User:', user);
-  prisma.$disconnect();
-});
